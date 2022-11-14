e26664a3ed97d264421fbdd77a11b00c4c33a7a9
ticholas_noler <tolern@student.psdr3.org>
2022-11-14 09:45:58 -0600

    Move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    Rigidbody rb;
     [SerializeField] float movementSpeed = 6f;
 
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput*movementSpeed, rb.velocity.y, verticalInput*movementSpeed);
+
 
     }
 }
