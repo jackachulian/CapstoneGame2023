de68c20db36c1fc3c272e58f51c462bbdcb33da1
David Luebbers <luebberd01@student.psdr3.org>
2022-11-14 09:56:35 -0600

    get pranked

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] float movementSpeed = 6f;
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 10f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
         
-
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput *movementSpeed);
     }
 }
