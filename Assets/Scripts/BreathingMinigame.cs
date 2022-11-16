6573b833c5f9d0c7c1a4b8b70dee346205c0e344
David Luebbers <luebberd01@student.psdr3.org>
2022-11-16 09:47:48 -0600

    Breath but best (so far)

--- Assets/Scripts/BreathingMinigame.cs
+++ Assets/Scripts/BreathingMinigame.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BreathingMinigame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+    [SerializeField] float speed = 1f;
+
     void Update()
     {
-        
+        if(Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f){
+            currentWaypointIndex ++;
+            if (currentWaypointIndex >= waypoints.Length){
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
     }
 }
