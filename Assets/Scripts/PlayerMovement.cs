0b2a85eae2a6c58b5516a45c9db1ab57ef9ff809
ticholas_noler <tolern@student.psdr3.org>
2022-11-18 10:04:34 -0600

    Oriented the right way

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float movementSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
