da65ae60e1941a8d2b0d34c9e9142bb916690201
caesarj <jackachulian@gmail.com>
2022-12-07 09:41:00 -0600

    Working cutscene  now (?)

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialog : MonoBehaviour
+public class Cutscene : MonoBehaviour
 {
 
     public TextMeshProUGUI textComponent;
