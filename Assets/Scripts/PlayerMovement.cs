85daf053968dc7e054717690b8ed24ca8ebbdb7d
caesarj <jackachulian@gmail.com>
2022-12-12 09:22:47 -0600

    Added posters

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float turnSpeed = 90f;
     [SerializeField] private Animator animator;
 
+    // If the player is sitting on a chair
+    private bool sitting;
+    // If the player is in transition between two locations (used for sitting, etc)
+    private bool inTransition;
+    // Player will continue to transition until this time is reached
+    private float transitionUntil;
+    // Speed to move when transitioning (Set on transition)
+    private float transitionSpeed;
+    // The position the player is transitioning towards
+    private Transform transitionDestination;
+    // The chair the player is on
+    private GameObject chair;
+    // Delay in seconds between positions when sitting down and standing up
+    public float sitDelay = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -41,5 +55,46 @@ public class PlayerMovement : MonoBehaviour
         }
 
         rb.velocity = direction;
+
+        // If in transition, move towards, and end transition if past time
+        if (inTransition) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transitionDestination.position, transitionSpeed*Time.deltaTime);
+            if (Time.time > transitionUntil)
+            {
+                inTransition = false;
+            }
+        }
+
+        // Interact
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Sit in chair if not sitting and not in transition
+            if (chair != null && !inTransition)
+            {
+                sitting = true;
+                animator.SetBool("sitting", sitting);
+                TransitionTo(chair.transform.Find("SitPos"), sitDelay);
+            }
+        }
+    }
+
+    void TransitionTo(Transform destination, float delay)
+    {
+        transitionUntil = Time.time + delay;
+        transitionSpeed = (destination.position - transform.position).magnitude / delay;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Chair"))
+        {
+            chair = collision.gameObject;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        chair = null;
     }
 }
