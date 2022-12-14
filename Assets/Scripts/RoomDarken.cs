ff00dd52437980ccdeba9588ddf9fea67625b1c3
Justout <jkajustin@gmail.com>
2022-12-14 10:03:14 -0600

    Rooms darken when you're not in them

---
+++ Assets/Scripts/RoomDarken.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDarken : MonoBehaviour
+{
+    private MeshRenderer ms;
+    void Start(){
+        ms = GetComponent<MeshRenderer>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with player");
+            ms.enabled = false;
+        }
+        else{
+            Debug.Log("Collided with non-player");
+        }
+    }
+
+    void OnTriggerExit(Collider collider){
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision exit with player");
+            ms.enabled = true;
+        }
+        else{
+            Debug.Log("Collision exit with non-player");
+        }
+    }
+
+}
