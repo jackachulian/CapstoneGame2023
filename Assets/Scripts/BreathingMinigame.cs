adcc278c109a822aa502c4a89bafb11e179ee071
David Luebbers <luebberd01@student.psdr3.org>
2022-12-16 09:58:21 -0600

    breathing type thing (final)

--- Assets/Scripts/BreathingMinigame.cs
+++ Assets/Scripts/BreathingMinigame.cs
@@ -7,11 +7,14 @@ public class BreathingMinigame : MonoBehaviour
 
     [SerializeField] GameObject[] waypoints;
     int currentWaypointIndex = 0;
-    [SerializeField] float speed = 120f;
+    [SerializeField] float speed = 100f;
     bool breathingIn = true;
     bool difficultyChangeMade = false;
+    bool gameOver = false;
     public GameObject thumbsUp;
     public GameObject thumbsDown;
+    public GameObject FinishedText;
+    public GameObject BottomText;
 
     
 
@@ -24,37 +27,41 @@ public class BreathingMinigame : MonoBehaviour
                 currentWaypointIndex = 0;
             }
         }
-        //MAKE DIFFICULTY RISE IF YOU DO NOTHING, ADD USER FEEDBACK TEXT (GOOD JOB KEEP GOING, NO THATS NOT IT, ETC.), END GAME TEXT
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Random.Range(speed, speed + 2) * Time.deltaTime);
     
         //if they hold the space key down while the circle is expanding it will decrease the speed,
         // holding it down while the circle is contracting will increase the speed and difficulty back up to where it began.
-        if (Input.GetKey("space")){
-            if (!difficultyChangeMade){
-                if (breathingIn){
-                    speed -= 5f;
-                    print("difficulty lowered");
-                    difficultyChangeMade = true;
-                    thumbsUp.SetActive(true);
-                    StartCoroutine(DelayedTurnOff(0.2f));
-                } else {
+        
+        if (!gameOver){
+            if (Input.GetKey("space")){
+                if (!difficultyChangeMade){
+                    if (breathingIn){
+                        speed -= 3f;
+                        difficultyChangeMade = true;
+                        thumbsUp.SetActive(true);
+                        StartCoroutine(DelayedTurnOff(0.25f));
+                    } else {
                     if (speed < 120){
                         speed += 5f;
-                    print("difficulty increased");
-                    difficultyChangeMade = true;
+                        difficultyChangeMade = true;
                     } else {
                         print("difficulty already max");
                         difficultyChangeMade = true;
                     }
                     thumbsDown.SetActive(true);
-                    StartCoroutine(DelayedTurnOff(0.2f));
+                    StartCoroutine(DelayedTurnOff(0.25f));
+                    }
                 }
             }
         }
+        
 
-        if (speed <= 25){
-            speed = 15;
+        if (speed <= 40){
             //end game
+            speed = 15;
+            gameOver = true;
+            FinishedText.SetActive(true);
+            BottomText.SetActive(false);
         }
 
     }
